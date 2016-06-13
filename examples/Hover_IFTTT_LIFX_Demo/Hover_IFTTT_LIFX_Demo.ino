69f576fadbba38893ca60eec60ccc68ec451b031
emranemran <emran.mah@gmail.com>
2016-06-13 09:07:11 -0700

    cleaning up IFTTT LIFX demo and adding NeoPixels

--- examples/Hover_IFTTT_LIFX_Demo/Hover_IFTTT_LIFX_Demo.ino
+++ examples/Hover_IFTTT_LIFX_Demo/Hover_IFTTT_LIFX_Demo.ino
@@ -80,16 +80,34 @@ HOVER 2.0 --------------------
 
 #include "application.h"
 #include "Hover.h"
+#include "neopixel/neopixel.h"
+
+SYSTEM_MODE(AUTOMATIC);
+
+/* ======================= Neopixel declarations ======================== */
+
+// IMPORTANT: Set pixel COUNT, PIN and TYPE
+#define PIXEL_COUNT 24
+#define PIXEL_PIN D3
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
+
+/* ======================= Hover declarations ======================== */
 
 // pin declarations for Hover
 int ts = 5;
 int res = 6;
 
+int delay1 = 40;
+int delay2 = 20;
+
 // Enable or disable different modes. Only gestures and taps are enabled by default.
 // Set the following four options to 0x01 if you want to capture every event. Note that the Serial console will be 'spammy'.
 #define GESTUREMODE 0x01    //0x00 = disable gestures, 0x01 = enable gestures
 #define TOUCHMODE 0x00      //0x00 = disable touch, 0x01 = enable touch
-#define TAPMODE 0x00        //0x00 = disable taps, 0x01 = enable taps, 0x02 = single taps only, 0x03 = double taps only
+#define TAPMODE 0x01        //0x00 = disable taps, 0x01 = enable taps, 0x02 = single taps only, 0x03 = double taps only
 #define POSITIONMODE 0x00   //0x00 = disable position tracking, 0x01 = enable position tracking
 
 // initialize Hover
@@ -104,47 +122,167 @@ void setup() {
   delay(2000);
   Serial.println("Initializing Hover...please wait.");
   hover.begin();
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop(void) {
+
+  unsigned long currentMillis = millis();    // used for updating 3D position coordinates. Set to update every 150ms by default.
+
+  // read incoming data stream from Hover
+  hover.readI2CData();
+
+  // retreive a gesture, touch, or position
+  Gesture g = hover.getGesture();
+  Touch t = hover.getTouch();
+  Position p = hover.getPosition();
+
+  // print Gesture data
+  if ( g.gestureID != 0){
+    Serial.print("Event: "); Serial.print(g.gestureType); Serial.print("\t");
+    Serial.print("Gesture ID: "); Serial.print(g.gestureID,HEX); Serial.print("\t");
+    Serial.print("Value: "); Serial.print(g.gestureValue,HEX); Serial.println("");
+    
+    if (g.gestureID == 1){
+        switch (g.gestureValue){
+            case 0x01:
+              Particle.publish("lifxleft", "swipeleft");
+              waveLeft();
+              break;
+            case 0x02:
+              Particle.publish("lifxright", "swiperight");
+              waveRight();
+              break;
+            case 0x03:
+              Particle.publish("lifxup","swipeup");            
+              waveDown();
+              break;
+            case 0x04:
+              Particle.publish("lifxdown","swipedown");            
+              waveUp();
+              break;
+            default:
+              break;
+        }
+    }
+    
+  }
+  
 }
 
 
 
-void loop(void) {
 
-    // read incoming data stream from Hover
-    hover.readI2CData();
+
+void waveUp(){
+  int brightness = 135;    
     
-    // retreive a gesture, touch, or position
-    Gesture g = hover.getGesture();
+  for(uint16_t i=4; i<12; i++) {
     
-    if (g.gestureID == 0x01){
-        
-        // if swipe gesture was detected
-        switch (g.gestureValue) {
-            case 0x01:
-                //swiped right
-                Particle.publish("lifx_action", "swipe_right", 60, PRIVATE);
-                Serial.println("swipe right");
-                break;
-            case 0x02:
-                //swiped left
-                Particle.publish("lifx_action", "swipe_left", 60, PRIVATE);
-                Serial.println("swipe left");
-                break;
-            case 0x03:
-                //swiped up
-                Particle.publish("lifx_action", "swipe_up", 60, PRIVATE);
-                Serial.println("swipe up");
-                break;
-            case 0x04:
-                //swiped down
-                Particle.publish("lifx_action", "swipe_down", 60, PRIVATE);
-                Serial.println("swipe down");
-                break;
-            default: 
-                break;
-        }    
-        
-    }
+    brightness = (i > 7) ? 255 : 135;
+    
+    strip.setColorDimmed(i, 0, 0, 200, brightness);
+    strip.setColorDimmed(23-i, 0, 0, 200, brightness);
+
+    strip.show();
+    delay(delay1);
+  }
+  
+  for(uint16_t i=4; i<12; i++) {
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+    strip.setPixelColor(23-i, strip.Color(0, 0, 0));
+    
+    strip.show();
+    delay(delay2);
+  }    
+    
+}
+
+void waveDown(){
+  int brightness = 135;    
+    
+  for(uint16_t i=8; i>0; i--) {
+    
+    brightness = (i < 4) ? 255 : 135;
+    
+    strip.setColorDimmed(i, 0, 0, 200, brightness);
+    strip.setColorDimmed(23-i, 0, 0, 200, brightness);
+
+    strip.show();
+    delay(delay1);
+  }
+  
+  for(uint16_t i=8; i>0; i--) {
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+    strip.setPixelColor(23-i, strip.Color(0, 0, 0));
+    
+    strip.show();
+    delay(delay2);
+  }    
+    
+}
 
 
+
+void waveRight(){
+  int brightness = 135;    
+    
+  for(uint16_t i=0; i<6; i++) {
+    
+    brightness = (i > 2) ? 255 : 135;
+    
+    strip.setColorDimmed(12+i, 0, 0, 200, brightness);
+    strip.setColorDimmed(23-i, 0, 0, 200, brightness);
+
+    strip.show();
+    delay(delay1);
+  }
+  
+  for(uint16_t i=0; i<6; i++) {
+    strip.setPixelColor(12+i, strip.Color(0, 0, 0));
+    strip.setPixelColor(23-i, strip.Color(0, 0, 0));
+    
+    strip.show();
+    delay(delay2);
+  }    
+    
+}
+
+void waveLeft(){
+  int brightness = 135;    
+    
+  for(uint16_t i=0; i<6; i++) {
+    
+    brightness = (i > 2) ? 255 : 135;
+    
+    strip.setColorDimmed(12-i, 0, 0, 200, brightness);
+    strip.setColorDimmed(i, 0, 0, 200, brightness);
+
+    strip.show();
+    delay(delay1);
+  }
+  
+  for(uint16_t i=0; i<6; i++) {
+    strip.setPixelColor(12-i, strip.Color(0, 0, 0));
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+    
+    strip.show();
+    delay(delay2);
+  }    
+    
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
 }
